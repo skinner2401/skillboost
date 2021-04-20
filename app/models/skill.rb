04f0cc40bf6b4e80d7bd5615a5560de3da05e3a5
Chris Amundson <christopher.b.amundson@gmail.com>
2021-04-20 09:16:46 -0700

    removing MVC for skills

--- app/models/skill.rb
+++
@@ -1,5 +0,0 @@
-class Skill < ApplicationRecord
-  belongs_to :assessment
-
-  validates :name, length: { minimum: 3 }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :assessments
-  resources :skills
   resources :users
   get 'skills/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
